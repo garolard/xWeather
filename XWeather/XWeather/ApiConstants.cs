372aeaa671a8891afcf1e40e59cbac1504274df2
Gabriel Ferreiro <gbril9119@gmail.com>
2016-06-25 23:43:30 +0200

    .

--- XWeather/XWeather/ApiConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace XWeather
-{
-    public static class ApiConstants
-    {
-        public const string WeatherBaseUri = "http://api.openweathermap.org/data/";
-        public const string WeatherApiVersion = "2.5";
-        public const string WeatherApiKey = "05d8d9907a681e57410865d231ddec7a";
-    }
-}
