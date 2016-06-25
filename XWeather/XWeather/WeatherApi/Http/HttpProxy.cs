808ca85828152ae628d70ac34e179ea6215a9e82
Gabriel Ferreiro <gbril9119@gmail.com>
2016-06-25 21:16:44 +0200

    Implemented HttpProxy

---
+++ XWeather/XWeather/WeatherApi/Http/HttpProxy.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using ModernHttpClient;
+
+namespace XWeather.WeatherApi.Http
+{
+    public class HttpProxy
+    {
+        private readonly HttpClient _client;
+        private static HttpProxy _instance;
+
+        private HttpProxy()
+        {
+            _client = new HttpClient(new NativeMessageHandler());
+        }
+
+        public static HttpProxy Instance => _instance ?? (_instance = new HttpProxy());
+
+        public async Task<string> Get(string targetUri)
+        {
+            var response = await _client.GetAsync(targetUri);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> Post(string targetUri, HttpContent postContent)
+        {
+            var response = await _client.PostAsync(targetUri, postContent);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
