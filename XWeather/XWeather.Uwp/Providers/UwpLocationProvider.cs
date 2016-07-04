26bcf6a46e39356031664b7d9b3a8d3205e64371
Gabriel Ferreiro <gbril9119@gmail.com>
2016-07-04 19:28:32 +0200

    Reimplemented UwpLocationProvider Re-implementation of UwpLocationProvider in an event driven manner

--- XWeather/XWeather.Uwp/Providers/UwpLocationProvider.cs
+++ XWeather/XWeather.Uwp/Providers/UwpLocationProvider.cs
@@ -8,18 +8,23 @@ namespace XWeather.Uwp.Providers
 {
     public class UwpLocationProvider : ILocationProvider
     {
-        public async Task<GeoLocation> GetCurrentLocationAsync()
+        private bool _isListening;
+        private Geolocator _locator;
+
+
+        public bool IsListening { get; }
+
+        public event EventHandler<PositionEventArgs> PositionChanged;
+
+
+        public async Task<GeoLocation> GetPositionAsync()
         {
             var accessStatus = await Geolocator.RequestAccessAsync();
 
             switch (accessStatus)
             {
                 case GeolocationAccessStatus.Allowed:
-                    var locator = new Geolocator()
-                    {
-                        DesiredAccuracyInMeters = 500
-                    };
-                    var position = await locator.GetGeopositionAsync();
+                    var position = await _locator.GetGeopositionAsync();
                     return new GeoLocation()
                     {
                         Latitude = position.Coordinate.Latitude,
@@ -28,6 +33,47 @@ namespace XWeather.Uwp.Providers
                 default:
                     return null;
             }
+        }
+
+        public Task<bool> StartListeningAsync(int minTime, double minDistance)
+        {
+            if (minTime < 0)
+                throw new ArgumentOutOfRangeException(nameof(minTime));
+            if (minDistance < 0)
+                throw new ArgumentOutOfRangeException(nameof(minDistance));
+            if (IsListening)
+                throw new InvalidOperationException();
+
+            _isListening = true;
+
+            var locator = _locator;
+            locator.ReportInterval = (uint) minTime;
+            locator.MovementThreshold = minDistance;
+            locator.PositionChanged += OnPositionChanged;
+
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> StopListeningAsync()
+        {
+            if (!IsListening)
+                return Task.FromResult(true);
+
+            _locator.PositionChanged -= OnPositionChanged;
+            _isListening = false;
+
+            return Task.FromResult(true);
+        }
+
+
+        private void OnPositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            var position = new GeoLocation()
+            {
+                Latitude = args.Position.Coordinate.Latitude,
+                Longitude = args.Position.Coordinate.Longitude
+            };
+            PositionChanged?.Invoke(this, new PositionEventArgs(position));
         }
     }
 }
