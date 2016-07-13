08774d76a931927e96e2c4ae4de6f6d2066649ab
Gabriel Ferreiro <gbril9119@gmail.com>
2016-07-13 20:44:03 +0200

    Added DayForecastDto for displaying 5-day forecast in main view

---
+++ XWeather/XWeather/Dto/DayForecastDto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace XWeather.Dto
+{
+    public class DayForecastDto : MvxViewModel
+    {
+        private double _maxTemp;
+        private double _mintemp;
+        private double _clouds;
+        private DateTime _forecastTime;
+
+
+        public DayForecastDto()
+        {
+            
+        }
+
+
+        public double MaxTemp
+        {
+            get { return _maxTemp; }
+            set { _maxTemp = value; RaisePropertyChanged(); }
+        }
+
+        public double Mintemp
+        {
+            get { return _mintemp; }
+            set { _mintemp = value; RaisePropertyChanged(); }
+        }
+
+        public double Clouds
+        {
+            get { return _clouds; }
+            set { _clouds = value; RaisePropertyChanged(); }
+        }
+
+        public DateTime ForecastTime
+        {
+            get { return _forecastTime; }
+            set { _forecastTime = value; RaisePropertyChanged(); }
+        }
+    }
+}
